21721f65bd87665a164364664d6278cbebf5f5d3
Maset0 <120063355+Maset0@users.noreply.github.com>
2022-12-08 13:38:06 +0200

    Update RobloxScript1.lua

--- RobloxScript1.lua
+++ RobloxScript1.lua
@@ -10,6 +10,6 @@ local Dupe = Window:NewTab("Dupe")
 local DupeSection = Dupe:NewSection("PSX-Dupe")
 
 
-MainSection:NewButton("Click To Activate!", "Script BY 8888 Team!", function()
+MainSection: NewButton("Click To Activate!", "Script BY 8888 Team!", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Maset0/dsdsdsd/main/info.lua"))()
 end)
